3061f8f75592b18f43f43f2348ce7853ab6c444e
ItzAaliyahh20 <maturanfrances123@gmail.com>
2026-01-31 02:03:17 +0800

    feature(dialogue):added objectives

---
+++ data/ObjectiveData.lua
@@ -0,0 +1,36 @@
+local ObjectiveData = {
+
+    obj1  = { prereq = "DialogueData.lua/tutorial", text = "Go to the Sink" },
+    obj2  = { prereq = "DialogueData.lua/scene1", text = "Go to Ducky’s Room" },
+    obj3  = { prereq = "DialogueData.lua/scene2", text = "Pick up the Picture Frame" },
+    obj4  = { prereq = "DialogueData.lua/scene3", text = "Go find Mom" },
+    obj5  = { prereq = "DialogueData.lua/scene4", text = "Get the food by the bed and feed Mom" },
+    obj6  = { prereq = "DialogueData.lua/scene5", text = "Go to the Lake outside" },
+    obj7  = { prereq = "DialogueData.lua/scene6", text = "Go back inside" },
+    obj8  = { prereq = "DialogueData.lua/scene7", text = "Locate the Journal" },
+    obj9  = { prereq = "DialogueData.lua/scene8", text = "Go to the Town Square" },
+    obj10 = { prereq = "mrandynpc", text = "Talk to Mrs. Andy" },
+    obj11 = { prereq = "shadyducknpc", text = "Talk to the Shady Duck" },
+    obj12 = { prereq = "kurtnpc", text = "Talk to Kurt" },
+    obj13 = { prereq = "ritanpc", text = "Talk to Rita" },
+    obj14 = { prereq = "DialogueData.lua/scene9", text = "Go to the Whisper Willows" },
+    obj15 = { prereq = "DialogueData.lua/scene10", text = "Find the Whisper Weed" },
+    obj16 = { prereq = "DialogueData.lua/scene11", text = "Go to the Ash Lands" },
+    obj17 = { prereq = "DialogueData.lua/scene12", text = "Find the Ashroot Bulb" },
+
+    -- Route 2: Trust Father’s Words
+    obj18 = { prereq = "DialogueRoute2.lua/scene2", text = "Go to the Hill with the Thousand Turns" },
+    obj19 = { prereq = "DialogueData.lua/scene13", text = "Find the Milkfish" },
+    obj20 = { prereq = "breadScene", text = "Buy Bread from the Shady Duck" },
+    obj21 = { prereq = "DialogueRoute2.lua/scene3", text = "Prepare the Cure" },
+    obj22 = { prereq = "DialogueRoute2.lua/scene4", text = "Feed Mom" },
+    obj23 = { prereq = "DialogueRoute2.lua/scene6", text = "Go check on Mom" },
+
+    -- Route 3: Ignore Him
+    obj24 = { prereq = "DialogueRoute3.lua/scene1", text = "Go to the Hill with the Thousand Turns" },
+    obj25 = { prereq = "DialogueData.lua/scene13", text = "Find the Milkfish" },
+    obj26 = { prereq = "DialogueRoute3.lua/scene2", text = "Prepare the Cure" },
+    obj27 = { prereq = "DialogueRoute3.lua/scene3", text = "Feed Mom" },
+    obj28 = { prereq = "DialogueRoute3.lua/scene4", text = "Go check on Mom" },
+
+}
